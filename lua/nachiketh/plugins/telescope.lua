95050433e4f217af28e9c2326fad46aae756806e
Nachiketh K S <nachikethks2000@gmail.com>
2023-11-08 22:02:44 +0530

    Changed telescope git files key binding

--- lua/nachiketh/plugins/telescope.lua
+++ lua/nachiketh/plugins/telescope.lua
@@ -25,7 +25,7 @@ return {
 
         telescope.load_extension("fzf")
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
         vim.keymap.set('n', '<leader>ps', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end)
